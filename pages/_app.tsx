6148358f0784d6bae724bf08dddc36ee870ab96b
derekprieur <DPrieur123@gmail.com>
2022-08-16 16:11:28 -0400

    updated tab info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import Head from 'next/head'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true)
@@ -16,6 +17,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+      <head>
+        <title>TikTik</title>
+        <link rel="icon" href="../public/favicon.ico" />
+      </head>
       <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
         <Navbar />
         <div className='flex gap-6 md:gap-20'>
